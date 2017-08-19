7956c07f3b1f7410f5e29c144806b47d080455f4
Daniel Pearson <faballion@gmail.com>
2017-08-19 17:55:47 +0200

    Add search form to home page

--- banner.php
+++ banner.php
@@ -1,12 +1,43 @@
 <div class="view intro hm-purple-light" style="">
 	<div class="full-bg-img flex-center">
+
 		<div class="container text-center white-text animated fadeIn">
-			<h2>Search Locations</h2>
-			<br>
-			<h5>It will always stay visible on the top, even when you scroll down</h5>
-			<p>Navbar's background will switch from transparent to solid color while scrolling down</p>
-			<br>
-			<p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+			<ul>
+                <li>
+                    <h1><strong>Search Locations</strong></h1>
+				</li>
+				<li>
+					<p>Enter either a search term or GPS coordinates</p>
+				</li>
+                <li>
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="md-form">
+                                <input type="text" id="form1" class="form-control">
+                                <label for="form1" class="active">Location Name</label>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="md-form">
+                                <input type="text" id="form2" class="form-control validate">
+                                <label for="form2" class="">Latitude</label>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="md-form">
+                                <input type="text" id="form3" class="form-control validate">
+                                <label for="form3" class="">Longitude</label>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="md-form">
+                                <button class="btn btn-lg purple darken-3 waves-effect waves-light">Search</button>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
 		</div>
+		
 	</div>
 </div>
